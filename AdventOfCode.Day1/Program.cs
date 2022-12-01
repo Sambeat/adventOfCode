1e27b237c664d263827f2815854ce32a6a20324e
scharade <minisylux@gmail.com>
2022-12-01 00:25:38 -0500

    Day 1

--- AdventOfCode.Day1/Program.cs
+++ AdventOfCode.Day1/Program.cs
@@ -1,3 +1,38 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 Console.WriteLine("Hello, World!");
+
+var caloriesLines = File.ReadAllLines("calories.txt");
+
+var caloriesTotals = new List<int>();
+
+var currentCaloriesSum = 0;
+foreach (var line in caloriesLines){
+  if (line == string.Empty)
+  {
+    caloriesTotals.Add(currentCaloriesSum);
+    currentCaloriesSum = 0;
+  }
+  else
+  {
+    var calories = int.Parse(line);
+    currentCaloriesSum += calories;
+  }
+}
+
+var maxCalories = caloriesTotals.Max();
+
+// Part 1 solution
+Console.WriteLine(maxCalories);
+
+caloriesTotals.Remove(maxCalories);
+
+var maxCalories2 = caloriesTotals.Max();
+
+caloriesTotals.Remove(maxCalories2);
+
+var maxCalories3 = caloriesTotals.Max();
+
+// Part 2 solution
+Console.WriteLine(maxCalories + maxCalories2 + maxCalories3);
+
