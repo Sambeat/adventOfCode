5305b188150f18ecd99ca82e331d0e1130e9ff86
scharade <minisylux@gmail.com>
2022-12-06 00:10:41 -0500

    Day 6

---
+++ AdventOfCode.Day6/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using AdventOfCode.Puzzles;
+
+Console.WriteLine("Hello, World!");
+
+var today = await Calendar.OpenPuzzleAsync(2022, 6);
+
+var lines = today.RawInput;
+
+Console.WriteLine(lines);
+Console.WriteLine(lines.Length);
+
+var start = 0;
+
+while (start <= lines.Length - 14)
+{
+    var lettersCount = lines.Substring(start, 14).Distinct().Count();
+
+    if (lettersCount == 14)
+    {
+        Console.WriteLine(lines.Substring(start, 14));
+        break;
+    }
+
+    start++;
+}
+
+Console.WriteLine(start + 14);
