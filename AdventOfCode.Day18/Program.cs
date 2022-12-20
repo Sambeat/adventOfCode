698fd40345ee89facc6b39aeafdbe6e2a8358ace
scharade <minisylux@gmail.com>
2022-12-20 02:37:14 -0500

    Day 18 + Day 20

--- AdventOfCode.Day18/Program.cs
+++ AdventOfCode.Day18/Program.cs
@@ -11,7 +11,7 @@ var lines = today.InputLinesTrimmed;
 
 var sides = new Dictionary<(double, double, double), int>();
 var exposedSides = new Dictionary<(int, int, int), int>();
-var cubes = new List<(int, int, int)>();
+var cubes = new List<(int x, int y, int z)>();
 
 var offsets = new (double, double, double)[]
     { (0, 0, 0.5), (0, 0.5, 0), (0.5, 0, 0), (0, 0, -0.5), (0, -0.5, 0), (-0.5, 0, 0) };
@@ -77,30 +77,93 @@ for (var i = 0; i < lines.Length; i++)
 Console.WriteLine($"exposed sides: {sides.Select(s => s.Value).Count(v => v == 0)}");
 Console.WriteLine($"exposed sides tech 2: {exposedSides.Sum(s => s.Value)}");
 
-var pockets = 0;
-for (var x = 0; x < 20; x++)
-{
-    for (var y = 0; y < 20; y++)
-    {
-        for (var z = 0; z < 20; z++)
-        {
-            if (!cubes.Contains((x, y, z)))
-            {
-                var cubeArounds = 0;
-                foreach (var (dx, dy, dz) in offsets)
-                {
-                    if (sides.ContainsKey((x + dx, y + dy, z + dz)))
-                        cubeArounds++;
-                }
+var boundedBoxMax = (cubes.Max(c => c.x), cubes.Max(c => c.y), cubes.Max(c => c.z));
+var boundedBoxMin = (cubes.Min(c => c.x), cubes.Min(c => c.y), cubes.Min(c => c.z));
 
-                if (cubeArounds == 6)
-                {
-                    pockets++;
-                }
+var boundedBox = new List<(int x, int y, int z)>();
+for (var x = boundedBoxMin.Item1 - 1; x <= boundedBoxMax.Item1 + 1; x++)
+{
+    for (var y = boundedBoxMin.Item2 - 1; y <= boundedBoxMax.Item2 + 1; y++)
+    {
+        for (var z = boundedBoxMin.Item3 - 1; z <= boundedBoxMax.Item3 + 1; z++)
+        {
+            var pos = (x, y, z);
+            boundedBox.Add(pos);
+        }
+    }
+}
+
+var airCubes = boundedBox.Except(cubes).ToHashSet();
+var startCube = airCubes.MinBy(c => c.x + c.y + c.z);
+
+var outsideAir = new HashSet<(int x, int y, int z)>();
+var faces = 0;
+
+buildByBFS(startCube);
+
+void buildByBFS((int x, int y, int z) sc)
+{
+    var visited = new HashSet<(int x, int y, int z)>();
+    var toVisit = new Queue<(int x, int y, int z)>();
+    toVisit.Enqueue(sc);
+    
+    while (toVisit.Any())
+    {
+        var nextToVisit = toVisit.Dequeue();
+    
+        if (cubes.Contains(nextToVisit) || visited.Contains(nextToVisit))
+        {
+            if (cubes.Contains(nextToVisit))
+                faces++;
+            continue;
+        }
+    
+        outsideAir.Add(nextToVisit);
+    
+        visited.Add(nextToVisit);
+    
+    
+        var neighbors = Neighbors(nextToVisit);
+    
+        foreach (var neighbor in neighbors)
+        {
+            if (!visited.Contains(neighbor))
+            {
+                
+                toVisit.Enqueue(neighbor);
             }
         }
     }
 }
 
-Console.WriteLine($"pockets: {pockets}");
-Console.WriteLine($"truly exposed sides: {sides.Select(s => s.Value).Count(v => v == 0) - (pockets * 6)}");
+Console.WriteLine($"True faces count: {faces}");
+
+IEnumerable<(int x, int y, int z)> Neighbors((int x, int y, int z) cube)
+{
+    var neighbors = new List<(int x, int y, int z)>();
+
+    // neighbors.Add((cube.x + 1, cube.y, cube.z));
+    // neighbors.Add((cube.x - 1, cube.y, cube.z));
+    // neighbors.Add((cube.x, cube.y + 1, cube.z));
+    // neighbors.Add((cube.x, cube.y - 1, cube.z));
+    // neighbors.Add((cube.x, cube.y, cube.z + 1));
+    // neighbors.Add((cube.x, cube.y, cube.z - 1));
+
+    for (var x = cube.x - 1; x <= cube.x + 1; x++)
+    {
+        for (var y = cube.y - 1; y <= cube.y + 1; y++)
+        {
+            for (var z = cube.z - 1; z <= cube.z + 1; z++)
+            {
+                if (x == cube.x && y == cube.y && z == cube.z)
+                    continue;
+
+                if (Math.Abs(x - cube.x) + Math.Abs(y - cube.y) + Math.Abs(z - cube.z) == 1 &&
+                    (airCubes.Contains((x, y, z)) || cubes.Contains((x, y, z))))
+                    neighbors.Add((x, y, z));
+            }
+        }
+    }
+
+    return neighbors;
+}
---
+++ AdventOfCode.Day20/Program.cs
@@ -0,0 +1,39 @@
+﻿using AdventOfCode.Events;
+
+var today = await Calendar.OpenPuzzleAsync(2022, 20);
+
+// today.PrintLines();
+
+var lines = today.InputLinesTrimmed;
+// var lines = File.ReadLines("test.txt").Where(s => string.Empty != s).ToArray();
+
+
+var initialEncryptedFile = lines.Select((l, i) => ((long)i, long.Parse(l) * 811589153)).ToList();
+var fileCopy = initialEncryptedFile.ToList();
+
+for (var mix = 0; mix < 10; mix++)
+{
+    for (var i = 0; i < initialEncryptedFile.Count; i++)
+    {
+        var currentValue = initialEncryptedFile[i];
+        var currentIndex = fileCopy.IndexOf(currentValue);
+
+        fileCopy.Remove(currentValue);
+
+        var insertionIndex =
+            (int)((currentIndex + currentValue.Item2 +
+                   fileCopy.Count * (Math.Abs(currentValue.Item2 / fileCopy.Count) + 1)) % fileCopy.Count);
+        fileCopy.Insert(insertionIndex, currentValue);
+    }
+}
+
+var indexOf0 = fileCopy.IndexOf(initialEncryptedFile.Single(l => l.Item2 == 0));
+var indexOfFirst = (indexOf0 + 1000) % fileCopy.Count;
+var indexOfSecond = (indexOf0 + 2000) % fileCopy.Count;
+var indexOfThird = (indexOf0 + 3000) % fileCopy.Count;
+
+Console.WriteLine($"{fileCopy[indexOfFirst]}, {fileCopy[indexOfSecond]}, {fileCopy[indexOfThird]}");
+
+var result = fileCopy[indexOfFirst].Item2 + fileCopy[indexOfSecond].Item2 + fileCopy[indexOfThird].Item2;
+
+Console.WriteLine(result);
