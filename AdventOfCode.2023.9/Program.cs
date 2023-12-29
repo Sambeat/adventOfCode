beddaba2e84713335c33942067bcb5cd2df10a57
Samuel Charade <git@scharade.net>
2023-12-29 00:03:18 -0500

    forgot actual files

---
+++ AdventOfCode.2023.9/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using AdventOfCode.Events;
+
+var today = await Calendar.OpenPuzzleAsync(2023, 9);
+
+var lines = today.InputLinesTrimmed;
