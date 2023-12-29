f991e78cb0a0fa20d60e998ebb7e83eb60ee4db1
Samuel Charade <git@scharade.net>
2023-12-28 23:51:47 -0500

    aoc 2023.8.2

--- AdventOfCode.2023.8/Program.cs
+++ AdventOfCode.2023.8/Program.cs
@@ -14,7 +14,7 @@ for (var l = 1; l < lines.Length; l++)
 {
     var parts = lines[l].Split("=");
 
-    var destinations = (parts[1].Substring(2, 3),parts[1].Substring(7,3));
+    var destinations = (parts[1].Substring(2, 3), parts[1].Substring(7, 3));
     mappings.Add(parts[0].Trim(), destinations);
 }
 
@@ -34,39 +34,60 @@ while (currentLocation != "ZZZ")
 
 Console.WriteLine(stepCount);
 
+var nodes = new Dictionary<string, Node>(mappings.Count);
 
-var currentLocations = lines.Where(line => line.Take(3).EndsWith(new []{'A'})).Select(line => new string(line.Take(3).ToArray())).ToList();
-var megaStepCount = 0;
+foreach (var key in mappings.Keys)
+{
+    nodes.Add(key, new Node() { Label = key });
+}
+
+foreach (var node in nodes.Values)
+{
+    node.Nodes.Add('L', nodes[mappings[node.Label].Item1]);
+    node.Left = nodes[mappings[node.Label].Item1];
+    node.Nodes.Add('R', nodes[mappings[node.Label].Item2]);
+    node.Right = nodes[mappings[node.Label].Item2];
+
+}
+
+var currentLocations = lines.Where(line => line.Take(3).EndsWith(new[] { 'A' }))
+    .Select(line => new string(line.Take(3).ToArray())).ToList();
+var currentNodes = currentLocations.Select(cl => nodes[cl]).ToArray();
+var megaStepCount = 0L;
 
 var stepsCounts = new int[currentLocations.Count];
 Array.Fill(stepsCounts, 0);
 
-var cycleCounts = new int[currentLocations.Count];
-Array.Fill(cycleCounts, 0);
-while (cycleCounts.Any(cl => cl == 0))
+var cycleCounts = new long[currentLocations.Count];
+Array.Fill(cycleCounts, 0L);
+
+var founds = 0;
+while (founds != currentNodes.Length)
 {
-    Parallel.For(0, currentLocations.Count, (i) =>
-    {
-        currentLocations[i] = instructions[stepCount % instructions.Length] switch
-        {
-            'L' => mappings[currentLocations[i]].Item1,
-            'R' => mappings[currentLocations[i]].Item2,
-            _ => throw new ArgumentOutOfRangeException()
-        };
+    // Parallel.For(0, currentLocations.Count, (i) =>
+    // {
+    //     currentLocations[i] = instructions[stepCount % instructions.Length] switch
+    //     {
+    //         'L' => mappings[currentLocations[i]].Item1,
+    //         'R' => mappings[currentLocations[i]].Item2,
+    //         _ => throw new ArgumentOutOfRangeException()
+    //     };
+    //
+    //     stepsCounts[i]++;
+    //     
+    //     if (currentLocations[i] == "FPZ" || currentLocations[i] == "SKZ" || currentLocations[i] == "ZZZ" || currentLocations[i] == "STZ" || currentLocations[i] == "MKZ" || currentLocations[i] == "CVZ")
+    //     {
+    //         Console.WriteLine($"Step {stepsCounts[i]} - {currentLocations[i]}");
+    //     }
+    //     
+    //     if (currentLocations[i].EndsWith(new[] { 'Z' }) && cycleCounts[i] == 0)
+    //     {
+    //         cycleCounts[i] = stepsCounts[i];
+    //     }
+    //
+    // });
 
-        stepsCounts[i]++;
-        
-        if (currentLocations[i] == "FPZ" || currentLocations[i] == "SKZ" || currentLocations[i] == "ZZZ" || currentLocations[i] == "STZ" || currentLocations[i] == "MKZ" || currentLocations[i] == "CVZ")
-        {
-            Console.WriteLine($"Step {stepsCounts[i]} - {currentLocations[i]}");
-        }
-        
-        if (currentLocations[i].EndsWith(new[] { 'Z' }) && cycleCounts[i] == 0)
-        {
-            cycleCounts[i] = stepsCounts[i];
-        }
 
-    });
     // for (var i = 0; i < currentLocations.Count; i++)
     // {
     //     currentLocations[i] = instructions[stepCount % instructions.Length] switch
@@ -81,14 +102,75 @@ while (cycleCounts.Any(cl => cl == 0))
     //         cycleCounts[i] = megaStepCount;
     //     }
     // }
-    
+    //
     // megaStepCount++;
+    //
+    // if (megaStepCount % 100000 == 0)
+    // {
+    //     Console.WriteLine(megaStepCount);
+    //     
+    // }
+
+
+    for (var i = 0; i < currentNodes.Length; i++)
+    {
+        // currentNodes[i] = currentNodes[i].Nodes[instructions[stepCount % instructions.Length]];
+        
+        currentNodes[i] = instructions[(int) megaStepCount % instructions.Length] switch
+        {
+            'L' => currentNodes[i].Left,
+            'R' => currentNodes[i].Right,
+            _ => throw new ArgumentOutOfRangeException()
+        };
+
+        if (currentNodes[i].Label[2] == 'Z' && cycleCounts[i] == 0)
+        {
+            cycleCounts[i] = megaStepCount + 1;
+            Console.WriteLine($"Found {i} = {megaStepCount + 1}");
+            founds++;
+        }
+    }
+
+    megaStepCount++;
+
+    if (megaStepCount % 100000000 == 0)
+    {
+        Console.WriteLine(megaStepCount);
+    }
 }
 
-var lcm = 1;
-foreach (var cycleCount in cycleCounts)
+// Parallel.For(0, currentLocations.Count, (i) =>
+// {
+//     while (cycleCounts[i] == 0)
+//     {
+//         currentNodes[i] = currentNodes[i].Nodes[instructions[stepCount % instructions.Length]];
+//         stepsCounts[i]++;
+//         if (currentNodes[i].Label[2] == 'Z' && cycleCounts[i] == 0)
+//         {
+//             cycleCounts[i] = stepsCounts[i];
+//             Console.WriteLine($"Found {i} = {stepsCounts[i]}");
+//         }
+//         
+//         if (stepsCounts[i] % 10000000 == 0)
+//      {
+//          Console.WriteLine($"{i}: {stepsCounts[i]}");
+//      }
+//     }
+// });
+
+Console.WriteLine(MathNet.Numerics.Euclid.LeastCommonMultiple(cycleCounts));
+
+class Node
 {
-    lcm = lcm * cycleCount;
-}
+    public string Label { get; set; }
 
-Console.WriteLine(lcm);
+    public Dictionary<char, Node> Nodes { get; set; }
+
+    public Node Left { get; set; }
+    public Node Right { get; set; }
+
+    public Node()
+    {
+        Nodes = new Dictionary<char, Node>(2);
+    }
+}
