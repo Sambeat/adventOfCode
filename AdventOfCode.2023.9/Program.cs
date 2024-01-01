e79a7188e5f7ccb49323d80ab01d3ce8963ad4cb
scharade <git@scharade.net>
2024-01-01 16:10:17 -0500

    aoc 2023.9.2

--- AdventOfCode.2023.9/Program.cs
+++ AdventOfCode.2023.9/Program.cs
@@ -6,4 +6,59 @@ var today = await Calendar.OpenPuzzleAsync(2023, 9);
 
 var lines = today.InputLinesTrimmed;
 
-today.PrintLines();
+var sum = 0;
+var backsum = 0;
+for (int i = 0; i < lines.Length; i++)
+{
+    var line = lines[i];
+
+    var numbers = line.Split(" ").Select(int.Parse).ToList();
+
+    var nextNumber = Differentiate(numbers);
+
+    sum += nextNumber;
+
+    backsum += DifferentiateBack(numbers);
+}
+
+Console.WriteLine(sum);
+
+Console.WriteLine(backsum);
+
+int Differentiate(List<int> series)
+{
+    if (series.All(i => i == 0))
+    {
+        return 0;
+    }
+
+    var nextSeries = new List<int>();
+
+    for (int i = 1; i < series.Count; i++)
+    {
+        nextSeries.Add(series[i] - series[i-1]);
+    }
+
+    var slope = Differentiate(nextSeries);
+
+    return series.Last() + slope;
+}
+
+int DifferentiateBack(List<int> series)
+{
+    if (series.All(i => i == 0))
+    {
+        return 0;
+    }
+
+    var nextSeries = new List<int>();
+
+    for (int i = 1; i < series.Count; i++)
+    {
+        nextSeries.Add(series[i] - series[i-1]);
+    }
+
+    var slope = DifferentiateBack(nextSeries);
+
+    return series.First() - slope;
+}
